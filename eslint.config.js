b71ae3035c4ae4304c43e01d27af5d51c029104c
Nadelopo <mistrmorsikk@mail.ru>
2025-07-03 20:22:50 +0400

    init pr

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  rules: {
+    'antfu/top-level-function': 'off',
+    'style/comma-dangle': 'off',
+    'ts/consistent-type-definitions': 'off',
+    '@stylistic/jsx-max-props-per-line': ['error', { maximum: 1, when: 'always' }],
+    '@stylistic/jsx-first-prop-new-line': ['error', 'multiline'],
+    'no-console': 'warn'
+  },
+  ignores: ['tsconfig.*', 'README.md'],
+
+})
