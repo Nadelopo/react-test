19edbd7f95ef7adf6e5a59d4d5065a9c37f3de0a
Nadelopo <mistrmorsikk@mail.ru>
2025-07-03 18:20:42 +0400

    Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Header } from '@/components/Header'
-import { PaintingsGrid } from './components/PaintingsGrid/PaintingsGrid'
-import { PaintingsPagination } from './components/PaintingsPagination/PaintingsPagination'
-import { SearchToolbar } from './components/SearchToolbar/SearchToolbar'
+import { PaintingsGrid } from './components/PaintingsGrid'
+import { PaintingsPagination } from './components/PaintingsPagination'
+import { SearchToolbar } from './components/SearchToolbar'
 import { useFiltersStore } from './stores/filtersStore'
 
-export const App = () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        gcTime: 60 * 1000 * 5,
-        staleTime: 60 * 1000 * 5
-      }
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 60 * 1000 * 5,
+      staleTime: 60 * 1000 * 5
     }
-  })
+  }
+})
 
+export const App = () => {
   const isLoading = useFiltersStore(state => state.isLoading)
   const totalPaintings = useFiltersStore(state => state.totalPaintings)
 
